f2789e9dd83140e069d8700577301c92cba2f6b8
Primitivo R. Montero <cibercafe_montero@hotmail.com>
2016-08-23 12:16:12 -0500

    update files

--- linked_list/test_linked.c
+++ linked_list/test_linked.c
@@ -1,14 +1,48 @@
 #include <stdio.h>
 #include "list.h"
 
+void list_print(List *list){
+  LIST_FOREACH(list, first, next, cur) {
+      printf("%i\n", (int)cur->value);
+  }
+}
+
 int main(int argc, char *argv[])
 {
+    int val;
     List* lista = List_create();
-    int i;
+    //test push
+    printf("%s\n", "test push");
+    List_push(lista, (void*) 0);
+    List_push(lista, (void*) 1);
+    List_push(lista, (void*) 2);
+    List_push(lista, (void*) 3);
+    List_push(lista, (void*) 4);
+    List_push(lista, (void*) 5);
+    list_print(lista);
+    //test pop
+    printf("%s\n", "test pop");
+    val = (int) List_pop(lista);
+    printf("pop value: %i\n", val);
+    list_print(lista);
+    printf("%s\n", "test unshift");
+    List_unshift(lista, 6);
+    list_print(lista);
+    printf("%s\n", "test shift");
+    val = (int) List_shift(lista);
+    list_print(lista);
+    printf("shift value: %i\n", val);
+    printf("%s\n", "test remove");
+    List_remove(lista, lista->first->next);
+    list_print(lista);
+    int cnt = List_count(lista);
+    printf("lista length: %i\n", cnt);
+
+    List_destroy(lista);
+    /*int i;
     for (i = 0; i < 1000000; ++i) {
         printf("%i\n", i);
         List_push(lista, (void*) i);
-    }
-
+    }*/
     return 0;
 }
