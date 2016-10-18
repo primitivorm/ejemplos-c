5dc2bd9608cb336c975bf43b68d38130fa7596a6
Primitivo R. Montero <cibercafe_montero@hotmail.com>
2016-10-18 16:00:20 -0500

    uf

--- parsers/ansi.c
+++ parsers/ansi.c
@@ -1,8 +1,10 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
 int main()
 {
+    /*
     char * s = "9474624707";
     long long int lo = 55559474624707;
     lo = strtoll(s, NULL, 0);
@@ -10,20 +12,25 @@ int main()
     const char * str = "hola mundo";
     unsigned int l      = strlen(str);
     printf("lenght of str: %i\n", l);
-
+    */
     //unsigned char c;
+    char c;
     int i;
     printf("\n%s\n", "unsigned========================================");
-    for (i = 0; i < 256; ++i)
+    for (i = 0; i < 256; i++)
     {
-        printf("%i=%c\t", i, i);
-    }
+        printf("%i = %c\t", c, c);
+        c = c+1;
+    }    
+    char euro[] = "€";
+    int l = strlen(euro);
+    printf("strlen: %d\n", l);
     /*
     printf("\n%s\n", "signed========================================");
     for (i = -128; i < 128; ++i)
     {
         printf("%i=%c\t", i, (unsigned char)i);
-    }
+    } 
     */
     /*printf("\n%s\n", "hola?");*/
     return 0;
