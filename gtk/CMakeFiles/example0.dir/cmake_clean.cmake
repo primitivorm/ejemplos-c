7cb7e546a20012a4f01e8f835ac31127835e1721
Primitivo R. Montero <cibercafe_montero@hotmail.com>
2016-12-02 11:23:37 -0600

    uf

---
+++ gtk/CMakeFiles/example0.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/example0.dir/example0.c.o"
+  "example0.pdb"
+  "example0"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/example0.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
